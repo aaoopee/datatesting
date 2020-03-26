f4200ec90d7d5ae4b7e012b71ffb1041c887a9ab
Antti Pelkonen <aaoopee@gmail.com>
2020-03-26 21:48:27 +0200

    Testing with Jupyter Labs and geocoding

--- pandatesting.py
+++ pandatesting.py
@@ -1,3 +1,12 @@
 
 import pandas
+import os
+from geopy.geocoders import ArcGIS
+nom = ArcGIS()
+
+print(os.listdir())
+
+df_excel = pandas.read_excel("original.xlsx", sheet_name=0)
+
+print(df_excel)
 
